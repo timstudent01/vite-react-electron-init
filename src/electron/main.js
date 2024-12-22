9da686fb26b46cb95679313b1be1e0de3f812282
Terry <terrychen@daoyidh.com>
2024-12-22 20:04:04 +0800

    inital

---
+++ src/electron/main.js
@@ -0,0 +1,9 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    width: 1366,
+    height: 768,
+  });
+  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+});
